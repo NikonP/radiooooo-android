f5fe5ca72d1a1511c0cb8ea54bf7cf141d19daaf
NikonP <podgorny.nikon@yandex.ru>
2020-12-03 16:28:51 +0300

    base

--- Timeyradio/app/src/main/java/com/example/timeyradio/ui/player/PlayerFragment.kt
+++ Timeyradio/app/src/main/java/com/example/timeyradio/ui/player/PlayerFragment.kt
@@ -1,4 +1,4 @@
-package com.example.timeyradio.ui.home
+package com.example.timeyradio.ui.player
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.timeyradio.R
 
-class HomeFragment : Fragment() {
+class PlayerFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var playerViewModel: PlayerViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        playerViewModel =
+                ViewModelProvider(this).get(PlayerViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_player, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        playerViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- Timeyradio/app/src/main/java/com/example/timeyradio/ui/player/PlayerViewModel.kt
+++ Timeyradio/app/src/main/java/com/example/timeyradio/ui/player/PlayerViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.timeyradio.ui.home
+package com.example.timeyradio.ui.player
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class PlayerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is player Fragment"
     }
     val text: LiveData<String> = _text
 }
