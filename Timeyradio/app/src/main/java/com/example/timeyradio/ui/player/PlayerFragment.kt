e1f5c1c5e697bfb88e22da7218e09a1f700e12d9
Fragers <48284978+Fragers@users.noreply.github.com>
2020-12-04 15:46:53 +0300

    1
    1

--- Timeyradio/app/src/main/java/com/example/timeyradio/ui/player/PlayerFragment.kt
+++ Timeyradio/app/src/main/java/com/example/timeyradio/ui/player/PlayerFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.timeyradio.R
-
+//выф
 class PlayerFragment : Fragment() {
 
     private lateinit var playerViewModel: PlayerViewModel
